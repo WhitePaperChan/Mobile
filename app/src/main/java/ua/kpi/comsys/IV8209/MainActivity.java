e76db36b25abb2c2eeb766a291d8c5f3e55d7030
WhitePaperChan <firerosealicorn@gmail.com>
2021-02-12 19:14:20 +0200

    Lab 1.1

---
+++ app/src/main/java/ua/kpi/comsys/IV8209/MainActivity.java
@@ -0,0 +1,41 @@
+package ua.kpi.comsys.IV8209;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TabLayout tabs = findViewById(R.id.tabs);
+        TextView textView = findViewById(R.id.textView);
+        tabs.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                if (tab.getPosition() == 0) {
+                    textView.setText(R.string.about);
+                } else {
+                    textView.setText("");
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+    }
+}
