118cfe3f0b658d443326f4b0cfedaa41a1bcbf91
WhitePaperChan <Firerosealicorn@gmail.com>
2021-04-05 01:46:40 +0300

    Lab4 is done

--- app/src/main/java/ua/kpi/comsys/IV8209/Book.java
+++ app/src/main/java/ua/kpi/comsys/IV8209/Book.java
@@ -6,10 +6,36 @@ public class Book {
     private String isbn13;
     private String price;
     private String image;
+    private String authors;
+    private String publisher;
+    private int pages;
+    private int year;
+    private int rating;
+    private String desc;
     public Book(String title, String subtitle, String isbn13, String price, String image){
         this.title = title;
         this.subtitle = subtitle;
         this.isbn13 = isbn13;
+        this.price = price;
+        this.image = image;
+    }
+    public Book(String title, String subtitle, String authors, String publisher, String isbn13,
+                int pages, int year, int rating, String desc, String price, String image){
+        this.title = title;
+        this.subtitle = subtitle;
+        this.authors = authors;
+        this.publisher = publisher;
+        this.isbn13 = isbn13;
+        this.pages = pages;
+        this.year = year;
+        if (rating > 5){
+            this.rating = 5;
+        } else if (rating < 1){
+            this.rating = 1;
+        } else {
+            this.rating = rating;
+        }
+        this.desc = desc;
         this.price = price;
         this.image = image;
     }
@@ -52,5 +78,53 @@ public class Book {
 
     public void setImage(String image) {
         this.image = image;
+    }
+
+    public String getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(String authors) {
+        this.authors = authors;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
     }
 }
--- app/src/main/java/ua/kpi/comsys/IV8209/ListActivity.java
+++ app/src/main/java/ua/kpi/comsys/IV8209/ListActivity.java
@@ -3,32 +3,27 @@ package ua.kpi.comsys.IV8209;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.constraintlayout.widget.ConstraintSet;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.SearchView;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
-
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.tabs.TabLayout;
-
 import org.json.*;
-
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-
 import java.io.*;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class ListActivity extends AppCompatActivity {
@@ -63,21 +58,76 @@ public class ListActivity extends AppCompatActivity {
 
             }
         });
-        Book[] books = this.loadBookArray();
+        ArrayList<Book> books = new ArrayList<Book>();
+        Collections.addAll(books, this.loadBookArray());
         TableLayout table = findViewById(R.id.table);
-        TableRow[] tableRows = new TableRow[books.length];
-        TextView[] bookInfo = new TextView[books.length];
-        ImageView[] bookImages = new ImageView[books.length];
+        TableRow[] tableRows = new TableRow[books.size()];
+        TextView[] bookInfo = new TextView[books.size()];
+        ImageView[] bookImages = new ImageView[books.size()];
         ConstraintLayout.LayoutParams params;
-        for (int i = 0; i < books.length; i++){
+
+        this.redrawTable(books, tableRows, bookImages, bookInfo, table, books);
+
+        SearchView search = findViewById(R.id.search);
+        search.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                ArrayList<Book> booksFiltered = new ArrayList<Book>();
+                table.removeAllViews();
+                int j = 0;
+                if (!newText.isEmpty()) {
+                    for (int i = 0; i < books.size(); i++) {
+                        if (books.get(i).getTitle().contains(newText)) {
+                            booksFiltered.add(books.get(i));
+                            j++;
+                        }
+                    }
+                    redrawTable(booksFiltered, tableRows, bookImages, bookInfo, table, books);
+                } else {
+                    redrawTable(books, tableRows, bookImages, bookInfo, table, books);
+                }
+                return false;
+            }
+        });
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ListActivity.this, AddBookActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    private void redrawTable(
+            ArrayList<Book> booksArrayFiltered, TableRow[] tableRows, ImageView[] bookImages, TextView[] bookInfo,
+            TableLayout table, ArrayList<Book> booksArrayUnfiltered
+    ){
+        tableRows = new TableRow[booksArrayFiltered.size()];
+        bookInfo = new TextView[booksArrayFiltered.size()];
+        bookImages = new ImageView[booksArrayFiltered.size()];
+        List<String> bookFiles = null;
+        try {
+            bookFiles = Arrays.asList(getAssets().list(""));
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+        for (int i = 0; i < booksArrayFiltered.size(); i++) {
             tableRows[i] = new TableRow(this);
             tableRows[i].setPadding(10, 10, 10, 10);
-            bookImages[i] = this.createBookImage(books[i]);
+            bookImages[i] = this.createBookImage(booksArrayFiltered.get(i));
             tableRows[i].addView(bookImages[i]);
             bookInfo[i] = new TextView(this);
             bookInfo[i].setText(
-                    books[i].getTitle() + "\n" + books[i].getSubtitle() + "\n" +
-                            books[i].getIsbn13() + "\n" + books[i].getPrice()
+                    booksArrayFiltered.get(i).getTitle() + "\n" + booksArrayFiltered.get(i).getSubtitle() + "\n" +
+                            booksArrayFiltered.get(i).getIsbn13() + "\n" + booksArrayFiltered.get(i).getPrice()
             );
             bookInfo[i].setLayoutParams(new TableRow.LayoutParams(
                     TableRow.LayoutParams.WRAP_CONTENT,
@@ -85,27 +135,72 @@ public class ListActivity extends AppCompatActivity {
             ));
             bookInfo[i].setPadding(10, 10, 10, 10);
             tableRows[i].addView(bookInfo[i]);
-            table.addView(tableRows[i]);
+            String isbn13 = booksArrayFiltered.get(i).getIsbn13();
+            TableRow tableRow = tableRows[i];
+            Book book = booksArrayFiltered.get(i);
+            if (bookFiles.contains(isbn13 + ".txt")) {
+                tableRows[i].setOnTouchListener(new OnSwipeTouchListener(ListActivity.this) {
+                    @Override
+                    public void onClick() {
+                        Intent intent = new Intent(ListActivity.this, BookInfoActivity.class);
+                        intent.putExtra("id", isbn13);
+                        startActivity(intent);
+                    }
 
+                    @Override
+                    public void onSwipeLeft() {
+                        swipeLeftMethod(table, tableRow, booksArrayUnfiltered, booksArrayFiltered, book);
+                    }
+                });
+            } else {
+                tableRows[i].setOnTouchListener(new OnSwipeTouchListener(ListActivity.this) {
+                    @Override
+                    public void onSwipeLeft() {
+                        swipeLeftMethod(table, tableRow, booksArrayUnfiltered, booksArrayFiltered, book);
+                    }
+                });
+            }
+        //}
+        table.addView(tableRows[i]);
+        }
+        if (booksArrayFiltered.isEmpty()){
+            TableRow tableRowNoBooks = new TableRow(this);
+            TextView textViewNoBooks = new TextView(this);
+            textViewNoBooks.setText("Книг не знайдено");
+            tableRowNoBooks.addView(textViewNoBooks);
+            table.addView(tableRowNoBooks);
         }
     }
 
     private Book[] loadBookArray() {
         String str = "";
+        JSONObject booksJson = null;
         try {
-            InputStream inputStream = getAssets().open("BooksList.txt");
-            int size = inputStream.available();
-            byte[] buffer = new byte[size];
-            inputStream.read(buffer);
+            ObjectInputStream inputStream = new ObjectInputStream(openFileInput("BooksListUser.txt"));
+            booksJson = new JSONObject((String) inputStream.readObject());
+        }
+        catch (IOException e) {
+            try {
+                InputStream inputStream = getAssets().open("BooksList.txt");
+                int size = inputStream.available();
+                byte[] buffer = new byte[size];
+                inputStream.read(buffer);
 
-            str = new String(buffer);
-            System.out.println(str);
-        } catch (IOException e){
+                str = new String(buffer);
+                booksJson = new JSONObject(str);
+                ObjectOutputStream stream = new ObjectOutputStream(openFileOutput("BooksListUser.txt", MODE_PRIVATE));
+                stream.writeObject(booksJson.toString());
+                stream.close();
+            } catch (IOException | JSONException e2) {
+                e.printStackTrace();
+            }
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
             e.printStackTrace();
         }
         Book[] books = new Book[0];
         try {
-            JSONObject booksJson = new JSONObject(str);
             JSONArray booksJsonArray = booksJson.getJSONArray("books");
             books = new Book[booksJsonArray.length()];
             JSONObject book;
@@ -132,7 +227,6 @@ public class ListActivity extends AppCompatActivity {
             Drawable drawable = Drawable.createFromStream(inputStream, null);
             bookImage.setImageDrawable(drawable);
         } catch (IOException e) {
-            //e.printStackTrace();
         }
         bookImage.setContentDescription(book.getTitle());
         bookImage.setScaleType(ImageView.ScaleType.FIT_CENTER);
@@ -144,5 +238,39 @@ public class ListActivity extends AppCompatActivity {
         ));
         return bookImage;
     }
+
+    private void swipeLeftMethod(TableLayout table,
+                                 TableRow tableRow,
+                                 ArrayList<Book> booksArrayUnfiltered,
+                                 ArrayList<Book> booksArrayFiltered,
+                                 Book book){
+        table.removeView(tableRow);
+        booksArrayUnfiltered.remove(book);
+        booksArrayFiltered.remove(book);
+        JSONArray booksJsonArray = new JSONArray();
+        for (int j = 0; j < booksArrayUnfiltered.size(); j++) {
+            try {
+                booksJsonArray.put(new JSONObject().
+                        put("title", booksArrayUnfiltered.get(j).getTitle()).
+                        put("subtitle", booksArrayUnfiltered.get(j).getSubtitle()).
+                        put("isbn13", booksArrayUnfiltered.get(j).getIsbn13()).
+                        put("price", booksArrayUnfiltered.get(j).getPrice()).
+                        put("image", booksArrayUnfiltered.get(j).getImage()));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        try {
+            JSONObject booksJson = new JSONObject().put("books", booksJsonArray);
+            ObjectOutputStream stream = new ObjectOutputStream(openFileOutput("BooksListUser.txt", MODE_PRIVATE));
+            stream.writeObject(booksJson.toString());
+            stream.close();
+        } catch (JSONException | FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
 
